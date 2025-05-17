7d6ad87818b9e897da11e1bcf4660a9011520f19
bexby <834687271947bexby@gmail.com>
2025-05-17 16:52:57 +0800

    third

--- get_topic_embedding.py
+++ get_topic_embedding.py
@@ -10,19 +10,20 @@ import pickle
 
 tokenizer = AutoTokenizer.from_pretrained("allenai/specter2_base")
 model = AutoAdapterModel.from_pretrained("allenai/specter2_base")
-# model.to("cuda")
 model.load_adapter("allenai/specter2", source="hf", load_as="proximity", set_active=True)
+model = model.to("cuda")
 body_folder = r"C:\vscode_project\latex_tokenizer\paper_body"
 paper_file = r"C:\vscode_project\latex_tokenizer\discrete_data\all_paper_info.jsonl"
 save_path = r"C:\vscode_project\latex_tokenizer\body_embedding"
-
+batch_size = 256
 
 def write_body_tensor(path, save_path):
     id_ls = []
     body = []
     part_index = 1
     file_list = os.listdir(path)
-    for file in tqdm(file_list, desc="imported files"):
+    for file in tqdm(file_list, desc="imported files", position=0):
+        res_emb = []
         with open(os.path.join(path, file), "r") as fr:
             lines = fr.readlines()
             for line in lines:
@@ -30,15 +31,20 @@ def write_body_tensor(path, save_path):
                 body.append(d["paragraph"])
                 id_ls.append({d["id"], d["paragraph_id"]})
 
-        inputs = tokenizer(body, padding=True, truncation=True,
-                                   return_tensors="pt", return_token_type_ids=False, max_length=512)
+        # inputs = tokenizer(body, padding=True, truncation=True,
+        #                            return_tensors="pt", return_token_type_ids=False, max_length=512)
         with torch.no_grad():
             # import ipdb
             # ipdb.set_trace()
             # output = model(**{k: v.to("cuda") for k, v in inputs.items()})
-            output = model(**inputs)
-            tensor_embeddings = output.last_hidden_state[:, 0, :]
-            torch.save(tensor_embeddings.cpu(), os.path.join(save_path, "embeddings_part" + str(part_index) + ".pt"))
+            for s in tqdm(range(len(body) // batch_size), position=1):
+                batch_inputs = tokenizer(body[s:s + batch_size], padding=True, truncation=True, return_tensors="pt", return_token_type_ids=False, max_length=512)
+                
+                output = model(**{k: v.to("cuda") for k, v in batch_inputs.items()})
+                tensor_embeddings = output.last_hidden_state[:, 0, :]
+                res_emb.append(tensor_embeddings.cpu())
+            
+            torch.save(torch.stack(res_emb), os.path.join(save_path, "embeddings_part" + str(part_index) + ".pt"))
             body = []
             part_index += 1
     
@@ -59,7 +65,7 @@ def write_paper_tensor(path, save_path):
         inputs = tokenizer(abstract, padding=True, truncation=True,
                                 return_tensors="pt", return_token_type_ids=False, max_length=512)
         with torch.no_grad():
-            output = model(**inputs)
+            output = model(**{k: v.to("cuda") for k, v in inputs.items()})
             tensor_embeddings = output.last_hidden_state[:, 0, :]
             torch.save(tensor_embeddings, os.path.join(save_path, "abstract_embedding.pt"))
 
@@ -68,4 +74,4 @@ def write_paper_tensor(path, save_path):
 
 
 write_body_tensor(body_folder, save_path)
-write_paper_tensor(paper_file, save_path)
+# write_paper_tensor(paper_file, save_path)
